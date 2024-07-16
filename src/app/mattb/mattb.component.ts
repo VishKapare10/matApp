333f41465fb116db0a1ba42c95b13d3f2cbb630e
VishKapare10 <vish10kapare@gmail.com>
2024-07-17 00:08:42 +0530

    UI changes.

--- src/app/mattb/mattb.component.ts
+++ src/app/mattb/mattb.component.ts
@@ -1,63 +1,75 @@
-import { Component, ViewChild } from '@angular/core';
-import { MatTableDataSource, MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow } from '@angular/material/table';
-import { MatSort } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatTableModule} from '@angular/material/table'
-import { MatInputModule } from '@angular/material/input';
-//import { MatSortModule } from '@angular/material/sort';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import { OnInit, AfterViewInit, Component, ViewChild } from '@angular/core';
+import { MatSort, Sort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import {MatCardModule} from '@angular/material/card';
+
 
 // app.component.ts
 
 export interface UserData {
-  id: string;
   name: string;
   age: number;
   gender: string;
 }
 
+const ELEMENT_DATA: UserData[] = [
+  { name: 'Abhay', age: 25, gender: 'Male' },
+  { name: 'John', age: 28, gender: 'Male' },
+  { name: 'Akash', age: 19, gender: 'Male' },
+  { name: 'Neha', age: 18, gender: 'Female' },
+  { name: 'Pratik', age: 21, gender: 'Male' },
+  { name: 'Sonal', age: 20, gender: 'Female' }
+];
+
+let originalData: UserData[];
+
 @Component({
-    selector: 'app-mattb',
-    templateUrl: './mattb.component.html',
-    styleUrl: './mattb.component.css',
-    standalone: true,
-    imports: [MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow]
+  selector: 'app-mattb',
+  templateUrl: './mattb.component.html',
+  styleUrl: './mattb.component.css',
+  standalone: true,
+  imports: [MatTableModule, MatSortModule,MatCardModule],
 })
-export class MattbComponent {
+export class MattbComponent implements OnInit, AfterViewInit {
+  @ViewChild(MatSort)
+  sort: MatSort = new MatSort();
 
+  displayedColumns: string[] = ['name', 'age', 'gender'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  filteredDataSource: UserData[] | undefined;
 
+  ngOnInit() {
+    originalData = this.dataSource.data;
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+  }
   Editrows() {
     throw new Error('Method not implemented.');
-    }
-    displayedColumns2: string[] = ['name', 'age', 'gender'];
-    dataSource2: UserData[] = [
-      { id: '1', name: 'John', age: 30, gender: 'Male' },
-      { id: '2', name: 'Alice', age: 25, gender: 'Female' },
-      { id: '3', name: 'Bob', age: 35, gender: 'Male' }
-    ];
-    filteredDataSource: UserData[] = this.dataSource2;
-  
-    applyFilter(event: Event, column: string) {
-      console.log('apply filter',column);
+  }
 
-      const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-      if (!filterValue) {
-        this.filteredDataSource = this.dataSource2;
-        return;
+  applyFilter(event: Event, column: string) {
+
+    const filterValue = (event.target as HTMLInputElement).value
+      .trim()
+      .toLowerCase();
+    if (!filterValue) {
+      this.filteredDataSource = originalData;
+      this.dataSource.data = originalData;
+      return;
+    }
+
+    this.dataSource.data = this.dataSource.data.filter((data) => {
+      if (column === 'name') {
+        return data[column].toLowerCase().includes(filterValue);
+      } else if (column === 'age') {
+        return data[column].toString().toLowerCase().includes(filterValue);
+      } else if (column === 'gender') {
+        return data[column].toLowerCase().includes(filterValue);
       }
-    
-      this.filteredDataSource = this.dataSource2.filter(data => {
-    if (column === 'name') {
-      return data[column].toLowerCase().includes(filterValue);
-    } else if (column === 'age') {
-      return data[column].toString().toLowerCase().includes(filterValue);
-    } else if (column === 'gender') {
-      return data[column].toLowerCase() === filterValue;
-    }
-        // Add a default return statement
-        return false; // or any other default value
-      });
-    }
-    
-    
+      // Add a default return statement
+      return false; // or any other default value
+    });
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,9 @@ import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, MatFormFieldModule, MatTableModule)]
+    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, MatFormFieldModule, MatTableModule,BrowserAnimationsModule)]
 })
   .catch(err => console.error(err));
